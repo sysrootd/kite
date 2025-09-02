dcc7fa0ec42405586c090cfb85a6440c4e1a7419
rakesh <alasyamrakesh77@gmail.com>
2025-09-03 01:08:52 +0530

    kernel files fixed and  added

--- src/kernel.c
+++ src/kernel.c
@@ -1,61 +1,78 @@
 #include "stm32f401.h"
 #include "kernel.h"
 
-
-void  SchedulerLaunch(void);
+void SchedulerLaunch(void);
 
 #define NUM_OF_THREADS  3        
-#define STACKSIZE   100      
+#define STACKSIZE       100      
 
-
-#define BUS_FREQ 				16000000
+#define BUS_FREQ        16000000
 uint32_t MILLIS_PRESCALER;
 
-struct tcb{
-  int32_t *stackPt;       
-  struct tcb *nextPt;  
+struct tcb {
+    int32_t *stackPt;       
+    struct tcb *nextPt;  
 };
-
 typedef struct tcb tcbType;
+
 tcbType tcbs[NUM_OF_THREADS];
 tcbType *currentPt;
 
 int32_t TCB_STACK[NUM_OF_THREADS][STACKSIZE];
 
-void KernelStackInit(int i){
-  tcbs[i].stackPt = &TCB_STACK[i][STACKSIZE-16]; 
-  TCB_STACK[i][STACKSIZE-1] = 0x01000000;  
+void KernelStackInit(int i) {
+    // Each thread stack pointer starts 16 registers below top
+    tcbs[i].stackPt = &TCB_STACK[i][STACKSIZE - 16];
+
+    // Simulated exception stack frame (auto-popped on thread start)
+    TCB_STACK[i][STACKSIZE - 1] = 0x01000000;  // xPSR (Thumb bit set)
+    TCB_STACK[i][STACKSIZE - 2] = 0x00000000;  // PC (will be filled later with task)
+    TCB_STACK[i][STACKSIZE - 3] = 0xFFFFFFFD;  // LR (return with PSP)
+    TCB_STACK[i][STACKSIZE - 4] = 0x12121212;  // R12
+    TCB_STACK[i][STACKSIZE - 5] = 0x03030303;  // R3
+    TCB_STACK[i][STACKSIZE - 6] = 0x02020202;  // R2
+    TCB_STACK[i][STACKSIZE - 7] = 0x01010101;  // R1
+    TCB_STACK[i][STACKSIZE - 8] = 0x00000000;  // R0
+
+    // Extra space for R4-R11 (manually saved by context switch)
+    TCB_STACK[i][STACKSIZE - 9]  = 0x11111111;
+    TCB_STACK[i][STACKSIZE - 10] = 0x10101010;
+    TCB_STACK[i][STACKSIZE - 11] = 0x09090909;
+    TCB_STACK[i][STACKSIZE - 12] = 0x08080808;
+    TCB_STACK[i][STACKSIZE - 13] = 0x07070707;
+    TCB_STACK[i][STACKSIZE - 14] = 0x06060606;
+    TCB_STACK[i][STACKSIZE - 15] = 0x05050505;
+    TCB_STACK[i][STACKSIZE - 16] = 0x04040404;
 }
 
+uint8_t KernelAddThreads(void(*task0)(void), void(*task1)(void), void(*task2)(void)) { 
+    __disable_irq();
 
+    tcbs[0].nextPt = &tcbs[1]; 
+    tcbs[1].nextPt = &tcbs[2]; 
+    tcbs[2].nextPt = &tcbs[0]; 
 
-uint8_t KernelAddThreads(void(*task0)(void),void(*task1)(void),void(*task2)(void))
-{ 
-	__disable_irq();
-  tcbs[0].nextPt = &tcbs[1]; 
-  tcbs[1].nextPt = &tcbs[2]; 
-  tcbs[2].nextPt = &tcbs[0]; 
-  KernelStackInit(0);
-  TCB_STACK[0][STACKSIZE-2] = (int32_t)(task0); 
-  
-  KernelStackInit(1);
-  TCB_STACK[1][STACKSIZE-2] = (int32_t)(task1); 
-  
-  KernelStackInit(2);
-  TCB_STACK[2][STACKSIZE-2] = (int32_t)(task2); 
-  currentPt = &tcbs[0];
-  __enable_irq();
+    KernelStackInit(0);
+    TCB_STACK[0][STACKSIZE - 2] = (int32_t)(task0);  // PC
+
+    KernelStackInit(1);
+    TCB_STACK[1][STACKSIZE - 2] = (int32_t)(task1);  // PC
+
+    KernelStackInit(2);
+    TCB_STACK[2][STACKSIZE - 2] = (int32_t)(task2);  // PC
+
+    currentPt = &tcbs[0];
+    __enable_irq();
 	
-  return 1;              
+    return 1;              
 }
-void KernelInit(void)
-{
-     __disable_irq();
-     MILLIS_PRESCALER=(BUS_FREQ/1000);
 
+void KernelInit(void) {
+    __disable_irq();
+    MILLIS_PRESCALER = (BUS_FREQ / 1000);
 }
-void KernelLaunch(uint32_t quanta)
-{
+
+void KernelLaunch(uint32_t quanta) {
     SysTick->CTRL = 0;
     SysTick->VAL  = 0;
     SysTick->LOAD = (quanta * MILLIS_PRESCALER) - 1;
@@ -69,31 +86,26 @@ void KernelLaunch(uint32_t quanta)
     SchedulerLaunch();
 }
 
-void ThreadYield(void)
-{
+void ThreadYield(void) {
     SysTick->VAL = 0;
     SCB->ICSR = SCB_ICSR_PENDSVSET_Msk;
 }
 
 uint32_t period_tick;
 
-void SemInit(int32_t *semaphore, int32_t value)
-{
-	*semaphore = value;
+void SemInit(int32_t *semaphore, int32_t value) {
+    *semaphore = value;
 }
 
-void SemPost(int32_t *semaphore)
-{
-	__disable_irq();
-	*semaphore += 0x01;
-	__enable_irq();
-}
-
-void SemWait(volatile int32_t *semaphore)
-{
+void SemPost(int32_t *semaphore) {
     __disable_irq();
-    while (*semaphore <= 0)
-    {   
+    *semaphore += 1;
+    __enable_irq();
+}
+
+void SemWait(volatile int32_t *semaphore) {
+    __disable_irq();
+    while (*semaphore <= 0) {   
         __enable_irq();
         ThreadYield();
         __disable_irq();
@@ -101,6 +113,3 @@ void SemWait(volatile int32_t *semaphore)
     *semaphore -= 1;
     __enable_irq();
 }
-
-
-
--- src/scheduler.s
+++ src/scheduler.s
@@ -2,41 +2,47 @@
     .cpu cortex-m4
     .thumb
 
-    .section .text
-    .align 2
     .global SysTick_Handler
     .global SchedulerLaunch
     .extern currentPt
 
-/* SysTick_Handler: save context and switch */
+/* -------------------------------------------------------
+ * SysTick_Handler: Context switch on SysTick interrupt
+ * -----------------------------------------------------*/
 SysTick_Handler:
-    cpsid   i
-    push    {r4-r11}            /* save r4-r11 */
-    ldr     r0, =currentPt      /* r0 -> &currentPt */
-    ldr     r1, [r0]            /* r1 = currentPt */
-    str     sp, [r1]            /* save SP */
-    ldr     r1, [r1, #4]        /* r1 = currentPt->next */
-    str     r1, [r0]            /* currentPt = r1 */
-    push    {r0, lr}
-    pop     {r0, lr}
-    ldr     r1, [r0]            /* r1 = currentPt */
-    ldr     sp, [r1]            /* SP = currentPt->stackPt */
-    pop     {r4-r11}            /* restore r4-r11 */
-    cpsie   i
+    cpsid   i                  @ disable interrupts
+    push    {r4-r11}           @ save callee-saved regs
+
+    ldr     r0, =currentPt     @ r0 = &currentPt
+    ldr     r1, [r0]           @ r1 = currentPt
+    str     sp, [r1]           @ save SP into TCB
+
+    ldr     r1, [r1, #4]       @ r1 = currentPt->next
+    str     r1, [r0]           @ currentPt = currentPt->next
+
+    ldr     r1, [r0]           @ r1 = currentPt (new thread)
+    ldr     sp, [r1]           @ SP = currentPt->stackPt
+
+    pop     {r4-r11}           @ restore callee-saved regs
+    cpsie   i                  @ enable interrupts
     bx      lr
 
-/* SchedulerLaunch: start first thread */
+/* -------------------------------------------------------
+ * SchedulerLaunch: Start first thread
+ * -----------------------------------------------------*/
 SchedulerLaunch:
     ldr     r0, =currentPt
-    ldr     r2, [r0]            /* r2 = currentPt */
-    ldr     sp, [r2]            /* SP = currentPt->stackPt */
-    pop     {r4-r11}
-    pop     {r0-r3}
+    ldr     r2, [r0]           @ r2 = currentPt
+    ldr     sp, [r2]           @ SP = currentPt->stackPt
+
+    mov     r0, sp
+    msr     psp, r0            @ set PSP = current stack pointer
+
+    pop     {r4-r11}           @ restore callee-saved regs
+    pop     {r0-r3}            @ restore caller-saved regs
     pop     {r12}
-    add     sp, sp, #4
-    pop     {lr}
-    add     sp, sp, #4
+    add     sp, sp, #4         @ skip LR (special EXC_RETURN value)
+    pop     {lr}               @ restore LR
+    add     sp, sp, #4         @ skip xPSR (already set)
     cpsie   i
     bx      lr
-
-    .align 2
