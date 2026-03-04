5cd3eaedfb8dc4e95249e3c118ae85e07ee5d26d
Rakesh Alasyam <alasyamrakesh77@gmail.com>
2026-03-04 21:01:34 +0530

    main.c updated

--- src/main.c
+++ src/main.c
@@ -1,38 +1,48 @@
 #include "rtos.h"
 #include "gpio.h"
 
+#define RED_LED     14
+#define GREEN_LED   13
+
+static semaphore_t sem_red;
+static semaphore_t sem_green;
+
 void task1_handler(void) {
     while (1) {
-        gpio_write(GPIOB, 14, 1);
+        semaphore_wait(&sem_red);
+        gpio_write(GPIOB, RED_LED, 1);
         task_delay(500);
-        gpio_write(GPIOB, 14, 0);
-        task_delay(500);
+        gpio_write(GPIOB, RED_LED, 0);
+        semaphore_post(&sem_green);
     }
 }
 
 void task2_handler(void) {
     while (1) {
-        gpio_write(GPIOB, 13, 1);
+        semaphore_wait(&sem_green);
+        gpio_write(GPIOB, GREEN_LED, 1);
         task_delay(200);
-        gpio_write(GPIOB, 13, 0);
-        task_delay(200);
+        gpio_write(GPIOB, GREEN_LED, 0);
+        semaphore_post(&sem_red);
     }
 }
 
 int main(void) {
 
-    gpio_init(GPIOB, 14, OUTPUT, PP, FAST, PU, 0);
-    gpio_init(GPIOB, 13, OUTPUT, PP, FAST, PU, 0);
+    gpio_init(GPIOB, RED_LED, OUTPUT, PP, FAST, PU, 0);
+    gpio_init(GPIOB, GREEN_LED, OUTPUT, PP, FAST, PU, 0);
 
-	create_task(0, task1_handler, 256U);
-	create_task(0, task2_handler, 256U);
+    /* alternating start: red semaphore initially available */
+    semaphore_init(&sem_red, 1);
+    semaphore_init(&sem_green, 0);
 
-	scheduler_init();
-	scheduler_start();
+    create_task(0, task1_handler, 256U);
+    create_task(0, task2_handler, 256U);
 
-
+    scheduler_init();
+    scheduler_start();
+    
     for (;;);
-
 }
 
 
