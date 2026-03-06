53c60f86e3d046317390461aba4be3649e1b00a6
Rakesh Alasyam <alasyamrakesh77@gmail.com>
2026-03-07 00:01:01 +0530

    comments added

--- inc/mem.h
+++ inc/mem.h
@@ -5,7 +5,9 @@
 #include <stddef.h>
 
 // Define here how much memory required
-// for your tcbs in bytes (static memory pool allocation to avoid malloc)  
+// for your tcbs in bytes (static memory pool allocation to avoid malloc)
+// Warning total bytes should align word size
+  
 #define TCB_POOL_SIZE   4096
 
 void *TCB_pool(ptrdiff_t incr);
