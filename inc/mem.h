f73553caba047a9ce88f7560c7db54b07f5e2a8c
Rakesh Alasyam <alasyamrakesh77@gmail.com>
2026-03-11 00:17:13 +0530

    return typeof TCB_pool added

--- inc/mem.h
+++ inc/mem.h
@@ -1,12 +1,13 @@
 #ifndef MEM_H
 #define MEM_H
 
+#include "sched.h"
 // (pre static memory pool allocation to avoid malloc)
 // Define here how much memory required for your tcbs in bytes
 // Warning: total bytes should align word
   
 #define TCB_POOL_SIZE   4096
 
-void *TCB_pool(ptrdiff_t incr);
+TCB_t* TCB_pool(ptrdiff_t incr);
 
 #endif
