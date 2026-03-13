479a71031ad29964e98c783986bef0713502d3ba
Rakesh Alasyam <alasyamrakesh77@gmail.com>
2026-03-13 21:52:18 +0530

    semaphore only code added to main.c

--- src/main.c
+++ src/main.c
@@ -12,21 +12,117 @@ static volatile int buffer[BUFFER_SIZE];
 static volatile int in  = 0;
 static volatile int out = 0;
 
-semaphore_t led_sem;
+// semaphore_t led_sem;
+
+// semaphore_t empty;
+// semaphore_t full;
+// mutex_t buffer_mutex;
+
+// void red_led_task(void)
+// {
+//     while (1)
+//     {
+//         semaphore_wait(&led_sem);
+//         gpio_write(GPIOB, RED_LED, 1);
+//         task_delay(1000);
+//         gpio_write(GPIOB, RED_LED, 0);
+//         semaphore_post(&led_sem);
+//     }
+// }
+
+// void green_led_task(void)
+// {
+//     while (1)
+//     {
+//         semaphore_wait(&led_sem);
+//         gpio_write(GPIOB, GREEN_LED, 1);
+//         task_delay(1000);
+//         gpio_write(GPIOB, GREEN_LED, 0);
+//         semaphore_post(&led_sem);
+//     }
+// }
+
+// void producer_task(void)
+// {
+//     int item = 0;
+
+//     while (1)
+//     {
+//         semaphore_wait(&empty);
+//         mutex_lock(&buffer_mutex);
+
+//         buffer[in] = item;
+//         uart_printf(USART2, "Produced %d\r\n", item);
+
+//         in = (in + 1) % BUFFER_SIZE;
+//         item++;
+
+//         mutex_unlock(&buffer_mutex);
+//         semaphore_post(&full);
+
+//     }
+// }
+
+// void consumer_task(void)
+// {
+//     int item;
+
+//     while (1)
+//     {
+//         semaphore_wait(&full);
+//         mutex_lock(&buffer_mutex);
+
+//         item = buffer[out];
+//         uart_printf(USART2, "Consumed %d\r\n", item);
+
+//         out = (out + 1) % BUFFER_SIZE;
+
+//         mutex_unlock(&buffer_mutex);
+//         semaphore_post(&empty);
+
+//     }
+// }
+
+// int main(void)
+// {
+
+//     //-----------system init--------------------
+//     gpio_init(GPIOB, RED_LED, OUTPUT, PP, FAST, PU, 0);
+//     gpio_init(GPIOB, GREEN_LED, OUTPUT, PP, FAST, PU, 0);
+
+//     uart_init(USART2, SYSTEM_CLK, BAUD_RATE);
+
+//     semaphore_init(&led_sem, 1);
+
+//     semaphore_init(&empty, BUFFER_SIZE);
+//     semaphore_init(&full, 0);
+//     mutex_init(&buffer_mutex);
+
+//     task_init(1, producer_task, 64U);
+//     task_init(1, consumer_task, 64U);
+//     task_init(1, red_led_task, 64U);
+//     task_init(1, green_led_task, 64U);
+
+//     scheduler_init();
+//     //------------------------------------------
+    
+//     scheduler_start();
+
+//     while (1);
+// }
+
 
 semaphore_t empty;
 semaphore_t full;
-mutex_t buffer_mutex;
 
 void red_led_task(void)
 {
     while (1)
     {
-        semaphore_wait(&led_sem);
         gpio_write(GPIOB, RED_LED, 1);
-        task_delay(1000);
+        task_delay(200);
         gpio_write(GPIOB, RED_LED, 0);
-        semaphore_post(&led_sem);
+        task_delay(200);
     }
 }
 
@@ -34,11 +130,10 @@ void green_led_task(void)
 {
     while (1)
     {
-        semaphore_wait(&led_sem);
         gpio_write(GPIOB, GREEN_LED, 1);
-        task_delay(1000);
+        task_delay(500);
         gpio_write(GPIOB, GREEN_LED, 0);
-        semaphore_post(&led_sem);
+        task_delay(500);
     }
 }
 
@@ -49,17 +144,13 @@ void producer_task(void)
     while (1)
     {
         semaphore_wait(&empty);
-        mutex_lock(&buffer_mutex);
 
         buffer[in] = item;
         uart_printf(USART2, "Produced %d\r\n", item);
-
         in = (in + 1) % BUFFER_SIZE;
         item++;
 
-        mutex_unlock(&buffer_mutex);
         semaphore_post(&full);
-
     }
 }
 
@@ -70,33 +161,25 @@ void consumer_task(void)
     while (1)
     {
         semaphore_wait(&full);
-        mutex_lock(&buffer_mutex);
 
         item = buffer[out];
         uart_printf(USART2, "Consumed %d\r\n", item);
-
         out = (out + 1) % BUFFER_SIZE;
 
-        mutex_unlock(&buffer_mutex);
         semaphore_post(&empty);
-
     }
 }
 
 int main(void)
 {
-
-    //-----------system init--------------------
+    //system init---------------------------------------
     gpio_init(GPIOB, RED_LED, OUTPUT, PP, FAST, PU, 0);
     gpio_init(GPIOB, GREEN_LED, OUTPUT, PP, FAST, PU, 0);
 
     uart_init(USART2, SYSTEM_CLK, BAUD_RATE);
 
-    semaphore_init(&led_sem, 1);
-
     semaphore_init(&empty, BUFFER_SIZE);
     semaphore_init(&full, 0);
-    mutex_init(&buffer_mutex);
 
     task_init(1, producer_task, 64U);
     task_init(1, consumer_task, 64U);
@@ -104,8 +187,7 @@ int main(void)
     task_init(1, green_led_task, 64U);
 
     scheduler_init();
-    //------------------------------------------
-    
+    //------------------------------------------------------
     scheduler_start();
 
     while (1);
